3f0cdff14b4d3d68effac76dc2d5a53e28f49b78
lydarras <lydarras@gmail.com>
2022-02-11 10:42:03 +0100

    Ajout de l'exo2

---
+++ exo2.php
@@ -0,0 +1,11 @@
+<?php
+
+$angle = 42;
+$x = cos($angle);
+$y = sin($angle);
+
+echo "Sachant que l'angle est ".$angle."°<br/>";
+echo "La position x est (".$x.",0) et y en (0,".$y.")<br/>";
+echo "P se positionne en x : $x et y : $y";
+
+?>
